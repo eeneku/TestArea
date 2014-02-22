f7015f1521c7b831b2233ef49ab7961c94cd81dd
eeneku <eeneku@gmail.com>
2014-02-22 16:53:44 +0200

    Added battle loop tests!

--- battle_loop_generator.py
+++ battle_loop_generator.py
@@ -15,14 +15,41 @@ unit_rules = [[0, -1, 1, -1, 1],
               [1, -1, 1, 0, -1],
               [-1, 1, -1, 1, 0]]
 
-player = [ROCK, PAPER, SCISSORS]
-enemy = [ROCK, PAPER, SCISSORS, PAPER, SCISSORS, SCISSORS]
+player_units = [ROCK, ROCK, LIZARD, SPOCK, SCISSORS]
+enemy_units = [ROCK, SPOCK, SCISSORS, PAPER, SPOCK, LIZARD]
 
 # Press A for AWESOME!
-result = itertools.permutations(player, len(player))
-usable = []
+player_permutations = itertools.permutations(player_units, len(player_units))
+enemy_permutations = itertools.permutations(enemy_units, len(enemy_units))
 
-for e in result:
-    usable.append(e)
+player_combos = []
+enemy_combos = []
 
-print(usable)
+for e in player_permutations:
+    player_combos.append(e)
+
+for e in enemy_permutations:
+    enemy_combos.append(e)
+
+player_wins = {}
+
+
+for en in enemy_combos:
+    for pl in player_combos:
+        player = list(pl)
+        enemy = list(en)
+        while len(enemy) > 0 and len(player) > 0:
+            if unit_rules[player[0]][enemy[0]] == 1:
+                enemy.pop(0)
+                player.append(player.pop(0))
+            elif unit_rules[player[0]][enemy[0]] == 0:
+                enemy.pop(0)
+                player.pop(0)
+            elif unit_rules[player[0]][enemy[0]] == -1:
+                player.pop(0)
+                enemy.append(enemy.pop(0))
+
+        if len(player) > 1:
+            player_wins.setdefault(en, set()).add(pl)
+
+print (player_wins)
